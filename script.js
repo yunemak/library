638b28382371289e94f2cb749a4b25b1382f960f
yunemak <yeak558@gmail.com>
2026-06-30 19:41:01 +0300

    Add book constructor, library addition function

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+function Book(name, author, pages, id) {
+	if (!new.target)
+		throw Error("You called a constructor without the keyword 'new'!");
+	this.name = name;
+	this.author = author;
+	this.pages = pages;
+	this.id = id;
+}
+
+function addBookToLibrary(books, name, author, pages) {
+	let id = crypto.randomUUID();
+	let book = new Book(name, author, pages, id);
+	books.push(book);
+}
+
+let books = [];
