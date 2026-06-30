f3871c8f16e7894e26f57b84680d1849031f71ff
yunemak <yeak558@gmail.com>
2026-06-30 20:24:43 +0300

    Add loop function that puts books to library

--- script.js
+++ script.js
@@ -14,10 +14,28 @@ function addBookToLibrary(books, name, author, pages) {
 }
 
 let books = [];
+const library = document.querySelector(".library");
+
 
 addBookToLibrary(books, "Warrior 1", "Ada", 453);
 addBookToLibrary(books, "Savior", "Bob", 341);
 addBookToLibrary(books, "Ipsum", "Lorem", 902);
 addBookToLibrary(books, "Warrior 2", "Ada", 572);
 
-books.forEach((book) => console.log(book))
+books.forEach((book) => {
+	let div = document.createElement("div");
+	div.classList.add("book");
+	let name = document.createElement("p");
+	name.textContent = book.name;
+	let author = document.createElement("p");
+	author.textContent = book.author;
+	let pages = document.createElement("p");
+	pages.textContent = book.pages;
+	let id = document.createElement("p");
+	id.textContent = book.id;
+	div.appendChild(name);
+	div.appendChild(author);
+	div.appendChild(pages);
+	div.appendChild(id);
+	library.appendChild(div);	
+});
