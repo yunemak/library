39a1ead7aac6accc15cb17eb96f353ae9b5de703
yunemak <yeak558@gmail.com>
2026-07-01 09:41:53 +0300

    Add is read feature

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function Book(name, author, pages, id) {
+function Book(name, author, pages, isRead, id) {
 	if (!new.target)
 		throw Error("You called a constructor without the keyword 'new'!");
 	this.name = name;
 	this.author = author;
 	this.pages = pages;
+	this.isRead = isRead;
 	this.id = id;
 }
 
-function addBookToLibrary(books, name, author, pages) {
+function addBookToLibrary(books, name, author, pages, isRead) {
 	let id = crypto.randomUUID();
-	let book = new Book(name, author, pages, id);
+	let book = new Book(name, author, pages, isRead, id);
 	books.push(book);
 }
 
@@ -19,10 +20,10 @@ const newBookBtn = document.querySelector("#new-book-btn");
 const form = document.querySelector("form");
 const addBookBtn = document.querySelector("#add-book-btn");
 
-addBookToLibrary(books, "Warrior 1", "Ada", 453);
-addBookToLibrary(books, "Savior", "Bob", 341);
-addBookToLibrary(books, "Ipsum", "Lorem", 902);
-addBookToLibrary(books, "Warrior 2", "Ada", 572);
+addBookToLibrary(books, "Warrior 1", "Ada", 453, true);
+addBookToLibrary(books, "Savior", "Bob", 341, true);
+addBookToLibrary(books, "Ipsum", "Lorem", 902, false);
+addBookToLibrary(books, "Warrior 2", "Ada", 572, false);
 
 books.forEach((book) => {
 	let div = document.createElement("div");
@@ -33,11 +34,14 @@ books.forEach((book) => {
 	author.textContent = book.author;
 	let pages = document.createElement("p");
 	pages.textContent = book.pages;
+	let isRead = document.createElement("p");
+	isRead.textContent = book.isRead ? "Yes": "No";
 	let id = document.createElement("p");
 	id.textContent = book.id;
 	div.appendChild(name);
 	div.appendChild(author);
 	div.appendChild(pages);
+	div.appendChild(isRead);
 	div.appendChild(id);
 	library.appendChild(div);	
 });
@@ -46,7 +50,10 @@ newBookBtn.addEventListener("click", function (e) {
 	form.style.display = "flex";
 	addBookBtn.style.display = "block";
 	newBookBtn.style.display = "none";	
-
-
-
 });
+
+addBookBtn.addEventListener("click", function (e) {
+	form.style.display = "none";
+	addBookBtn.style.display = "none";
+	newBookBtn.style.display = "block";
+})
