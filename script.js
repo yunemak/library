0f2c3a0b0fcd5baa1d3c9881883c942379c6fd8f
yunemak <yeak558@gmail.com>
2026-07-01 11:16:58 +0300

    Add delete button

--- script.js
+++ script.js
@@ -9,50 +9,10 @@ const bookNameInput = document.querySelector("#bookName");
 const bookAuthorInput = document.querySelector("#bookAuthor");
 const bookPagesInput = document.querySelector("#bookPages");
 
-function Book(name, author, pages, isRead, id) {
-	if (!new.target)
-		throw Error("You called a constructor without the keyword 'new'!");
-	this.name = name;
-	this.author = author;
-	this.pages = pages;
-	this.isRead = isRead;
-	this.id = id;
-}
-
-function addBookToLibrary(books, name, author, pages, isRead) {
-	let id = crypto.randomUUID();
-	let book = new Book(name, author, pages, isRead, id);
-	books.push(book);
-	let div = document.createElement("div");
-	div.classList.add("book");
-	let namePara = document.createElement("p");
-	namePara.textContent = book.name;
-	let authorPara = document.createElement("p");
-	authorPara.textContent = book.author;
-	let pagesPara = document.createElement("p");
-	pagesPara.textContent = book.pages;
-	let isReadPara = document.createElement("p");
-	isReadPara.textContent = isRead;
-	let idPara = document.createElement("p");
-	idPara.textContent = book.id;
-	div.appendChild(namePara);
-	div.appendChild(authorPara);
-	div.appendChild(pagesPara);
-	div.appendChild(isReadPara);
-	div.appendChild(idPara);
-	library.appendChild(div);
-}
-
-function cleanInputs() {
-	bookNameInput.value = "";
-	bookAuthorInput.value = "";
-	bookPagesInput.value = "";
-}
-
-addBookToLibrary(books, "Warrior 1", "Ada", 453, true);
-addBookToLibrary(books, "Savior", "Bob", 341, true);
-addBookToLibrary(books, "Ipsum", "Lorem", 902, false);
-addBookToLibrary(books, "Warrior 2", "Ada", 572, false);
+addBookToLibrary(books, "Warrior 1", "Ada", 453, "Yes");
+addBookToLibrary(books, "Savior", "Bob", 341, "No");
+addBookToLibrary(books, "Ipsum", "Lorem", 902, "Yes");
+addBookToLibrary(books, "Warrior 2", "Ada", 572, "No");
 
 newBookBtn.addEventListener("click", function (e) {
 	form.style.display = "grid";
@@ -69,5 +29,58 @@ addBookBtn.addEventListener("click", function (e) {
 	cleanInputs();
 });
 
+// Functions
 
+function Book(name, author, pages, isRead, id) {
+	if (!new.target)
+		throw Error("You called a constructor without the keyword 'new'!");
+	this.name = name;
+	this.author = author;
+	this.pages = pages;
+	this.isRead = isRead;
+	this.id = id;
+}
 
+function addBookToLibrary(books, name, author, pages, isRead) {
+	let id = crypto.randomUUID();
+	let book = new Book(name, author, pages, isRead, id);
+	books.push(book);
+	let div = document.createElement("div");
+	div.classList.add("book");
+	addDeleteBtn(div);
+	addBookInfo(div, book);
+}
+
+function addDeleteBtn(div) {
+	let deleteBtn = document.createElement("button");
+	deleteBtn.textContent = "delete";
+	deleteBtn.addEventListener("click", function (e) {
+		div.remove();
+	});
+	div.appendChild(deleteBtn);
+}
+
+function addBookInfo(div, book) {
+	let namePara = document.createElement("p");
+	namePara.textContent = book.name;
+	let authorPara = document.createElement("p");
+	authorPara.textContent = book.author;
+	let pagesPara = document.createElement("p");
+	pagesPara.textContent = book.pages;
+	let isReadPara = document.createElement("p");
+	isReadPara.textContent = book.isRead;
+	let idPara = document.createElement("p");
+	idPara.textContent = book.id;
+	div.appendChild(namePara);
+	div.appendChild(authorPara);
+	div.appendChild(pagesPara);
+	div.appendChild(isReadPara);
+	div.appendChild(idPara);
+	library.appendChild(div);
+}
+
+function cleanInputs() {
+	bookNameInput.value = "";
+	bookAuthorInput.value = "";
+	bookPagesInput.value = "";
+}
